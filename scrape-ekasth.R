13d1ecd02f583d4c04311449acb1c42211d0328b
georgios.karamanis@me.com <georgios.karamanis@me.com>
2019-09-23 13:48:31 +0000

    Try this

--- scrape-ekasth.R
+++ scrape-ekasth.R
@@ -13,4 +13,8 @@ results <- html_table(tbls[[2]])
 colnames(results) <- (c("date", "time", "venue", "teams", "score", "referee_1", "referee_2"))
 
 results <- results %>% 
-  filter(score != "Σκορ")
+  filter(score != "Σκορ") %>% 
+  group_by(date)
+
+
+# https://stackoverflow.com/questions/39825429/how-to-see-n-rows-after-a-specific-row
