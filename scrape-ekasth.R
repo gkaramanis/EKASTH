dce70c49b8897949490cf88e43b7fc8f0b02235a
georgios.karamanis@me.com <georgios.karamanis@me.com>
2019-09-23 13:08:06 +0000

    Rename columns, add warning about git pull!

--- scrape-ekasth.R
+++ scrape-ekasth.R
@@ -1,4 +1,7 @@
+# git pull!
+
 library(rvest)
+library(tidyverse)
 
 url <- "http://www.ekasth.gr/champs/2012/champ200.htm"
 
@@ -7,3 +10,7 @@ webpage <- read_html(url)
 tbls <- html_nodes(webpage, "table")
 
 results <- html_table(tbls[[2]])
+colnames(results) <- (c("date", "time", "venue", "teams", "score", "referee_1", "referee_2"))
+
+results <- results %>% 
+  filter(score != "Σκορ")
